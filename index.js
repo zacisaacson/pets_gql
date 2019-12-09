75c751d20250606918f0a7d02a4a03f3d18d018b
Dione Wilson <dionew1@gmail.com>
2019-12-09 14:56:11 -0700

    Complete graphql queries

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 // Allows express to understand graphql and interact with graphql API
 // It is used as middleware for a single route
 const graphqlHTTP = require('express-graphql')
-
+const schema = require('./lib/schema/schema')
 
 app.use('/graphql-pets', graphqlHTTP({
-
+  schema: schema,
+  graphiql: true
 }))
 
 
