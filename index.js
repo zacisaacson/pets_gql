0454f774d481ca4490348a084082390a29bc3544
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-04 12:01:42 -0700

    Complete Query and Mutation lesson

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 // Allows express to understand graphql and interact with graphql API
 // It is used as middleware for a single route
 const graphqlHTTP = require('express-graphql')
+const schema = require('./lib/schema/schema')
 
 
 app.use('/graphql-pets', graphqlHTTP({
-
+  schema: schema,
+  //graphiql allows us to query from our browser
+  graphiql: true
 }))
 
 
